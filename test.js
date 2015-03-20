5a1067a5e3a1bc7877795fe0a8f4abde51fce02c
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-19 21:51:51 -0400

    lint

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*!
  * object-length <https://github.com/jonschlinkert/object-length>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
